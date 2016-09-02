b1e5c8528ef266c91e3bcfe4bb2de3770419b106
po_taka <angel.koilov@gmail.com>
2016-09-02 13:34:31 +0300

    db structure fixed

--- README.md
+++ README.md
@@ -4,3 +4,20 @@
 [![Build Status](https://travis-ci.org/angelk/tmp-rest.svg?branch=master)](https://travis-ci.org/angelk/tmp-rest)
 
 Database settings are in `config/db.php`. See `config/db.php.dist` for example
+
+Database init
+```
+CREATE TABLE `news` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `title` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `date` date NOT NULL,
+  `text` text CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16;
+
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`);
+
+--
+ALTER TABLE `news`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+```
--- Test/behat/features/bootstrap/database/restInitialDbState/init.sql
+++ Test/behat/features/bootstrap/database/restInitialDbState/init.sql
@@ -4,14 +4,12 @@ CREATE TABLE `news` (
   `id` int(10) UNSIGNED NOT NULL,
   `title` varchar(255) CHARACTER SET utf8 NOT NULL,
   `date` date NOT NULL,
-  `text` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-INSERT INTO `news` (`id`, `title`, `date`, `text`) VALUES
-(1, 'title 1', '2016-08-31', 'testText');
+  `text` text CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16;
 
 ALTER TABLE `news`
   ADD PRIMARY KEY (`id`);
 
+--
 ALTER TABLE `news`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
